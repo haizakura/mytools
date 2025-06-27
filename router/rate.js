7d0799b073be92e5e83283ab8a0efc6b5820ca74
HAIZAKURA <hhbilly99@gmail.com>
2025-06-27 15:14:51 +0900

    fix: api error

--- router/rate.js
+++ router/rate.js
@@ -22,49 +22,35 @@ const router = express.Router();
  * request.query = year, month, day, transCur, baseCur
  * response.body = { 'data': '' }
  */
-router.get('', (req, res) => {
+router.get('/rate', (req, res, next) => {
     try {
-        let year = req.query.year;
-        let month = req.query.month;
-        let day = req.query.day;
-        let transCur = req.query.transCur;
-        let baseCur = req.query.baseCur;
-        if (!year || !month || !day) {
-            let today = new Date();
-            year = today.getFullYear();
-            month = today.getMonth() + 1;
-            month = month < 10 ? '0' + month : month;
-            day = today.getDate();
-            day = day < 10 ? '0' + day : day;
-        }
-        if (!transCur || !baseCur) {
-            transCur = 'JPY';
-            baseCur = 'CNY';
-        }
+        const today = new Date();
+        const year = req.query?.year ?? today.getFullYear();
+        const month = req.query?.month ?? (today.getMonth() + 1).toString().padStart(2, '0');
+        const day = req.query?.day ?? today.getDate().toString().padStart(2, '0');
+        const transCur = req.query?.transCur ?? 'JPY';
+        const baseCur = req.query?.baseCur ?? 'CNY';
         axios.get('https://www.unionpayintl.com/upload/jfimg/' + year + month + day + '.json')
             .then(data => {
-                let exchangeRateJson = data.data.exchangeRateJson;
+                const exchangeRateJson = data.data?.exchangeRateJson;
                 exchangeRateJson.forEach(element => {
-                    if (element.transCur == transCur & element.baseCur == baseCur) {
+                    if (element.transCur === transCur & element.baseCur === baseCur) {
                         res.status(200).json({
                             data: element,
                         });
-                        return;
                     }
                 });
             })
             .catch(error => {
-                logger.error('Error:', error);
+                logger.error(error);
                 res.sendStatus(400);
-                return;
             });
     } catch (error) {
-        logger.error('Error:', error);
+        logger.error(error);
         res.sendStatus(502);
-        return;
     }
 });
 
 /** Rate Router End **/
 
-export { router as rate_router };
+export { router as rateRouter };
